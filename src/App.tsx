669914daf7211fc77f1e16f8c608939d4c79fd74
DorvilleThiago <103829961+DorvilleThiago@users.noreply.github.com>
2023-02-27 18:38:15 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { xml2js } from 'xml-js';
-import { convertXmlToObject } from "./XmlConvert";
+import {parseString} from "xml"
 
 function App() {
 
@@ -8,76 +7,69 @@ function App() {
     word: string;
     xml: string;
   }
-
   const [query, setQuery] = useState('');
-  const [prefix, setPrefix] = useState(null);
+  const [prefix, setPrefix] = useState([]);
   const [final, setFinal] = useState<DictionaryEntry[] | null>(null);
-  const [title, setTitle] = useState('Digite uma palavra!');
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+
 
   const handleInputChange = (event: any) => {
     setQuery(event.target.value);
   };
 
   useEffect(() => {
-    if (query.length > 2)
-    fetch(`https://api.dicionario-aberto.net/prefix/${query}`)
-      .then(response => response.json())
-      .then(prefix => setPrefix(prefix))
+    if (query.length > 2) {
+      fetch(`https://api.dicionario-aberto.net/prefix/${query}`)
+        .then(response => response.json())
+        .then(prefix => setPrefix(prefix.slice(0, 5)));
+    } else {
+      setPrefix([]);
+    }
   }, [query]);
 
   useEffect(() => {
-    console.log(prefix)
-  }, [prefix]);
-
-  useEffect(() => {
-    console.log(final)
-    if (final != null) {
+    if (final === null) {
+    }
+    else if (final?.length === 0) {
+      alert('palavra não encontrada!')
+    }
+    else {
       const wordTitle = final[0].word
       const upperTitle = wordTitle.charAt(0).toUpperCase() + wordTitle.slice(1); 
       setTitle(upperTitle)
+
+      let parser = new DOMParser();
+      let xmldoc = parser.parseFromString(final[0].xml, 'text/xml');
+      let def = xmldoc.getElementsByTagName('def')[0].textContent?.toString();
+      setDesc(def ?? '');
+
+      
     }
   }, [final]);
-
-  const elements = [
-    { id: 1, name: 'Apple' },
-    { id: 2, name: 'Banana' },
-    { id: 3, name: 'Cherry' },
-    { id: 4, name: 'Durian' },
-    { id: 5, name: 'Elderberry' },
-  ];
-
-
-  
-  
-  const filteredElements = query
-    ? elements.filter((element) =>
-        element.name.toLowerCase().includes(query.toLowerCase())
-      )
-    : [];
   
     function SearchWord(word: string) {
       if (word.length > 0) {
         word = word.toLowerCase()
-        console.log(word)
         fetch(`https://api.dicionario-aberto.net/word/${word}`)
           .then(response => response.json())
           .then(final => setFinal(final))
+          .catch(error => console.error('Error fetching data:', error))
       }
     }
-    
 
   return (
     <div className="h-screen bg-gradient-to-br from-blue-500 to-purple-500 flex justify-center items-center ">
 
-      <div className="relative w-4/5 h-4/5 bg-black bg-opacity-75 rounded-3xl shadow-big flex items-center flex-col overflow-x-hidden overflow-y-auto " >
+      <div id='box' className="relative w-4/5 h-4/5 bg-black bg-opacity-75 rounded-3xl shadow-big flex items-center flex-col overflow-x-hidden overflow-y-auto" >
 
-        <a target="_blank" href="https://www.thiago-dorville.tech" className="top-4 right-5 absolute w-28 h-10 bg-gradient-to-br from-blue-500 to-purple-500 flex items-center justify-center rounded-3xl">
+        <a target="_blank" href="https://www.thiago-dorville.tech" className="top-4 right-5 absolute w-28 h-10 bg-gradient-to-br from-blue-500 to-purple-500 flex items-center justify-center rounded-3xl hover:scale-105 transition-all">
           <p className="font-bold">Portfólio</p>
         </a>
 
         <div className="flex items-center mt-20 ">
           <i className="fa-solid fa-book text-6xl mr-6"></i>
-          <h1 className="text-7xl font-extrabold">Dicionário</h1>
+          <h1 className="text-7xl font-extrabold cursor-default">Dicionário</h1>
         </div>
 
         <div className="mt-10 z-20 flex">
@@ -86,17 +78,19 @@ function App() {
           <button onClick={() => SearchWord(query)} id='button'className="h-10 w-12 ml-3 bg-green-400 rounded-3xl"><i className="fa-solid fa-magnifying-glass  text-white text-2xl"></i></button>
         </div>
         
-        <div className="">
-          <ul className="absolute bg-white -translate-x-36 -translate-y-6 pt-7 pb-1 rounded-lg ">
-            {filteredElements.map((element: any) => (
-              <li className="text-black font-medium w-72 pl-2" key={element.id}>{element.name}</li>
-            ))}
+        <div style={{marginRight: 60}} className="absolute mt-56">
+          <ul className=" bg-white -translate-y-6 pt-8 pb-1 rounded-xl "> 
+            {
+              prefix.map((item,index) => (  
+                <li key={index} onClick={() => { SearchWord(item.word); setPrefix([]) }} className="text-black font-medium w-72 pl-2 cursor-pointer hover:bg-gray-500">{item.word}</li>
+              ))
+            }
           </ul>
         </div>
 
-        <div className="mt-5 w-4/5 ">
-          <h1 className="text-6xl font-semibold">{title}</h1>
-          <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae soluta asperiores repellat et nesciunt minima, hic voluptas molestiae quibusdam molestias quo qui, quod iure blanditiis repudiandae! Recusandae ut iusto veritatis.</p>
+        <div id='box' className="mt-14 w-4/5 animate-wiggle">
+          <h1 className="text-6xl font-semibold cursor-default">{title}</h1>
+          <p className="mt-5 text-lg font-thin cursor-default">{desc}</p>
         </div>
 
       </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,20 @@ module.exports = {
     extend: {
       boxShadow: {
         'big': 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px'
+      },
+      animation: {
+        wiggle: 'wiggle 0.3s ease-in-out',
+        appear: 'appear 0.3s ease-in-out foward',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        },
+        appear: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
       }
     },
   },
